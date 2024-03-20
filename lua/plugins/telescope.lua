6a82944830b1fdb87c501794d52ded02bfc73695
Harvey <nqphu1998@gmail.com>
2024-03-20 11:08:51 +0700

    feat: update telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,23 @@
 return {
   "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      path_display = { "truncate" },
+      sorting_strategy = "ascending",
+      layout_config = {
+        horizontal = {
+          prompt_position = "top",
+          preview_width = 0.55,
+        },
+        vertical = {
+          mirror = false,
+        },
+        width = 0.87,
+        height = 0.80,
+        preview_cutoff = 120,
+      },
+    },
+  },
   keys = {
     {
       "<leader>fw",
