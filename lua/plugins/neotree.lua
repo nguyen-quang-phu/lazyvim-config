8012c40fbbcd777533d8fd81f52526c3465cae91
Harvey <nqphu1998@gmail.com>
2024-03-20 18:15:55 +0700

    fix: update config for neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,8 +1,38 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  cmd = "Neotree",
+  enabled = true,
+  lazy = false,
+  keys = {
+    {
+      "<leader>e",
+      "<cmd>Neotree focus<cr>",
+      desc = "NEOTREE: focus",
+    },
+  },
+  deactivate = function()
+    vim.cmd([[Neotree close]])
+  end,
+  init = function()
+    if vim.fn.argc(-1) == 1 then
+      local stat = vim.loop.fs_stat(vim.fn.argv(0))
+      if stat and stat.type == "directory" then
+        require("neo-tree")
+      end
+    end
+  end,
   opts = {
+    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
     window = {
       mappings = {
+        ["<space>"] = "none",
         ["l"] = "open",
         ["f"] = function()
           vim.api.nvim_exec("Neotree focus filesystem left", true)
@@ -13,37 +43,44 @@ return {
         ["s"] = function()
           vim.api.nvim_exec("Neotree focus git_status left", true)
         end,
-      },
-    },
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        show_hidden_count = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-        hide_by_name = {
-          ".git",
-          ".DS_Store",
-          "thumbs.db",
+        ["Y"] = {
+          function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.setreg("+", path, "c")
+          end,
+          desc = "copy path to clipboard",
         },
-        never_show = {},
       },
     },
-    event_handlers = {
-      {
-        event = "file_opened",
-        handler = function(file_path)
-          require("neo-tree.command").execute({ action = "close" })
-        end,
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
       },
     },
-    keys = {
-      {
-        "<leader>e",
-        "<cmd>Neotree focus<cr>",
-        mode = "n",
-        desc = "NEOTREE: focus",
-      }
-    }
   },
+  config = function(_, opts)
+    local function on_move(data)
+      Util.lsp.on_rename(data.source, data.destination)
+    end
+
+    local events = require("neo-tree.events")
+    opts.event_handlers = opts.event_handlers or {}
+    vim.list_extend(opts.event_handlers, {
+      { event = events.FILE_MOVED, handler = on_move },
+      { event = events.FILE_RENAMED, handler = on_move },
+    })
+    require("neo-tree").setup(opts)
+    vim.api.nvim_create_autocmd("TermClose", {
+      pattern = "*lazygit",
+      callback = function()
+        if package.loaded["neo-tree.sources.git_status"] then
+          require("neo-tree.sources.git_status").refresh()
+        end
+      end,
+    })
+  end,
 }
