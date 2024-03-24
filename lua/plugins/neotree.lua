c46057c38ff238a9341548d9f1f5cc163c96c272
Harvey <nqphu1998@gmail.com>
2024-03-24 10:21:17 +0700

    feat: add end wise

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -26,9 +26,26 @@ return {
     sources = { "filesystem", "buffers", "git_status", "document_symbols" },
     open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
     filesystem = {
-      bind_to_cwd = false,
-      follow_current_file = { enabled = true },
-      use_libuv_file_watcher = true,
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".git",
+          ".DS_Store",
+          "thumbs.db",
+        },
+        never_show = {},
+      },
+    },
+    event_handlers = {
+      {
+        event = "file_opened",
+        handler = function(file_path)
+          require("neo-tree.command").execute({ action = "close" })
+        end,
+      },
     },
     window = {
       mappings = {
