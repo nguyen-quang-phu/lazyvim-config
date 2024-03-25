f4420c59aa87e8213c3002cb88060448af2eb284
Harvey <nqphu1998@gmail.com>
2024-03-25 12:15:11 +0700

    feat: add git plugin

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -3,9 +3,17 @@ return {
     "folke/noice.nvim",
     opts = {
       notify = {
-        enabled = false,
+        enabled = true,
+      },
+      message = {
+        view_error = false,
+      },
+      lsp = {
+        message = {
+          enabled = false,
+        },
       },
     },
   },
-  { "rcarriga/nvim-notify", enabled = false },
+  { "rcarriga/nvim-notify", enabled = true },
 }
