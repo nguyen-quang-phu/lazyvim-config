bb83f5ec00dac76e074fd6191648c65d96418370
Harvey <nqphu1998@gmail.com>
2024-03-31 14:56:30 +0700

    feat: add debug in ruby

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -3,13 +3,21 @@ return {
   optional = true,
   dependencies = {
     "nvim-lua/plenary.nvim",
-    -- "antoinemadec/FixCursorHold.nvim",
     "nvim-treesitter/nvim-treesitter",
     "olimorris/neotest-rspec",
   },
   opts = {
     adapters = {
-      ["neotest-rspec"] = {},
+      ["neotest-rspec"] = {
+        -- NOTE: By default neotest-rspec uses the system wide rspec gem instead of the one through bundler
+        rspec_cmd = function()
+          return vim.tbl_flatten({
+            "bundle",
+            "exec",
+            "rspec",
+          })
+        end,
+      },
     },
   },
 }
